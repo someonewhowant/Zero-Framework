94dcb836f38aa7d2f9bf6355936cc12fdc83211e
someonewhowant <seralexg95@gmail.com>
2026-03-28 11:33:28 +0200

    утиль для создания DOM узла

--- core.js
+++ core.js
@@ -1,4 +1,13 @@
+/*
+ * h function - creates a virtual DOM node
+ * @param {string} tag - HTML tag name
+ * @param {object} props - HTML attributes
+ * @param {...*} children - Child nodes
+ * @returns {object} Virtual DOM node
+ */
+function h(tag, props, ...children) {
+  return { tag, props: props || {}, children: children.flat() };
+}
 
 
-const app = document.getElementById("app");
-mount(new App(), app);
+console.log(h("div", { id: "app" }, h("h1", null, "Hello World")));
